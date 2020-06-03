815223a2c401b2e0f4c767a0a083b8ee04853fcc
demkantor <demkantor@gmail.com>
2020-06-02 22:32:08 -0500

    get and post routes set and tested with postman

--- routes/stores.router.js
+++ routes/stores.router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getStores } = require('../controlers/stores');
+const { getStores, addStore } = require('../controlers/stores');
 
 
 // get stores
-router.route('/').get(getStores);
-
+router
+.route('/')
+.get(getStores)
+.post(addStore);
 
 
 module.exports = router;
